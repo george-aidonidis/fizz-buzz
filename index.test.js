0b1da746afc0ffdb959831ba0a464b685fcb7926
George Aidonidis <george.aidonidis@gmail.com>
2021-02-11 09:50:30 +0200

    Project setup
    * Add development tools to ease up the implementation

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const fizzBuzz = require('.')
+
+describe('fizzBuzz', () => {
+	it.todo('returns Fizz when %3 = 0')
+
+	it.todo('returns Buzz when %5 = 0')
+
+	it.todo('returns FizzBuzz when both %3 = 0, %5 = 0')
+
+	it.todo('returns number on any other case')
+})
